5efb89f6e0f7555e74be0dda3fca2431ea23360e
cattuongtu <cattuongtu3@gmail.com>
2023-12-03 16:22:52 -0800

    First commit

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+namespace ProduceInSeasonApi.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsFruit { get; set; }
+        public string? Description { get; set; }
+        public string? Season { get; set; }
+        public string? Secret { get; set; }
+    }
+}
